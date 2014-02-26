a9199c23af0357db047ffe10f09e837888e3be7b
Ben Hughes <bwghughes@gmail.com>
2014-02-26 21:23:39 +0000

    Not sure about this one  - requests.exceptions.ConnectionError ot Timeout

--- tests/test_roomyjob.py
+++ tests/test_roomyjob.py
@@ -15,15 +15,16 @@ import requests
 
 from roomyjob import send_heartbeat, send_event, InvalidImageException
 
-ROOT_DEVICE_URL = "https://roomy.firebaseio.com/device"
+ROOT_DEVICE_URL = "https://test-url"
 TEST_DEVICE = "test-device"
+HEADERS = {'Content-Type': 'application/json'}
 
 
 @httpretty.activate
 def test_heartbeat_returns_ok_on_valid_body():
     httpretty.register_uri(httpretty.PATCH,
                            "{}/{}/last-seen.json".format(ROOT_DEVICE_URL,
-                                                     TEST_DEVICE),
+                                                         TEST_DEVICE),
                            body=json.dumps({"last_seen": 12345678}),
                            content_type="application/json")
 
@@ -49,7 +50,8 @@ def test_we_can_post_a_valid_event():
 def test_we_can_post_an_invalid_image_event():
     image_url = "https://roomy-pics-blah.com/1"
     httpretty.register_uri(httpretty.POST,
-                           "{}/{}/events/".format(ROOT_DEVICE_URL, TEST_DEVICE),
+                           "{}/{}/events/".format(ROOT_DEVICE_URL,
+                                                  TEST_DEVICE),
                            body=json.dumps({"image": image_url,
                                             "timestamp": 123456567}),
                            content_type="application/json")
@@ -59,13 +61,14 @@ def test_we_can_post_an_invalid_image_event():
 
 @httpretty.activate
 def test_we_error_on_event_post_connection_timeout():
-    image_url = "https://roomy-pics-blah.com/1.jpg"
+    image_url = "http://roomy-pics-blah.com/1.jpg"
     httpretty.register_uri(httpretty.POST,
-                           "{}/{}/events/".format(ROOT_DEVICE_URL, TEST_DEVICE),
+                           "{}/{}/events/".format(ROOT_DEVICE_URL,
+                                                  TEST_DEVICE),
                            body=json.dumps({"image": image_url,
                                             "timestamp": 123456567}),
                            content_type="application/json")
-    with raises(requests.exceptions.Timeout):
+    with raises(requests.exceptions.ConnectionError):
         send_event(ROOT_DEVICE_URL, TEST_DEVICE, image_url, 0)
 
 
@@ -77,5 +80,5 @@ def test_we_error_on_heartbeat_patch_connection_timeout():
                            body=json.dumps({"last_seen": 12345678}),
                            content_type="application/json")
 
-    with raises(requests.exceptions.Timeout):
+    with raises(requests.exceptions.ConnectionError):
         send_heartbeat(ROOT_DEVICE_URL, "test-device", 0)
