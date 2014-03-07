d9265442db4f970eef67d7de74c72a84026071e0
Ben Hughes <bwghughes@gmail.com>
2014-03-07 13:00:47 +0000

    Added versiotn to hertbeat

--- roomyjob/roomyjob.py
+++ roomyjob/roomyjob.py
@@ -7,6 +7,8 @@ import requests
 import logging
 logging.basicConfig()
 
+from . import __version__ as version
+
 HEADERS = {'Content-Type': 'application/json'}
 
 
@@ -24,7 +26,8 @@ def send_heartbeat(root_url, device_id=None, timeout=3):
     print "Sending heartbeat to {}".format(url)
     try:
         response = requests.patch(url, data=json.dumps({'ts': time.time(),
-                                                        'ip': get_ipaddress()}),
+                                                        'ip': get_ipaddress(),
+                                                        'version': version}),
                                   timeout=timeout, headers=HEADERS)
         return response
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError),\
