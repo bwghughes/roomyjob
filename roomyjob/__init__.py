8641eed63fd7698bacbed0140c53cd03d77fc67c
Ben Hughes <bwghughes@gmail.com>
2014-03-07 12:31:32 +0000

    Up version

--- roomyjob/__init__.py
+++ roomyjob/__init__.py
@@ -22,7 +22,7 @@ Options:
 
 __author__ = 'Ben Hughes'
 __email__ = 'bwghughes@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from roomyjob import send_heartbeat, send_event, InvalidImageException
 
